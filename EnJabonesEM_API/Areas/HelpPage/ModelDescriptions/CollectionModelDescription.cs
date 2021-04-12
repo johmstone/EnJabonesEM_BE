cdc5b6066283d48537ec7ba117378d270a1d6956
johmstone <61640703+johmstone@users.noreply.github.com>
2021-04-11 19:43:48 -0600

    Initial Commit

---
+++ EnJabonesEM_API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace EnJabonesEM_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ EnJabonesEM_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnJabonesEM_API.WebApiApplication" Language="C#" %>
